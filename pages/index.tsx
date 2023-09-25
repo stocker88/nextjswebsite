1911577b720f8096e3638402f61c89cc10d216b6
stocker88 <aness.hussein.ali@icloud.com>
2023-09-25 17:52:29 +0200

    working

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,26 +34,25 @@ export default function Index({ allPosts }: Props) {
        <Layout>
        <Home/>
         <Head>
-          <title>HelloStocker: AI Financial Advisor App</title>
+          <title>HelloStocker: AI Financial Advisor and Virtual Trading App</title>
           <meta charSet="utf-8" />
-          <meta name="description" property="og:description" content="Stocker AI App is an Investing Advisory App that helps you design strategies and practise them with virtual trading! Learn how to make money and earn passive income with Stocker Ai App" />
+          <meta name="description" property="og:description" content="Design budgeting and investing strategies your AI Financial Advisor and practise them with virtual trading! Learn how to make money and earn passive income with HelloStocker Ai App" />
           <meta name="facebook-domain-verification" content="8yjz6qlp98g8lnhvo6zso6lywe0037" />
           <link rel="shortcut icon" href="favicon.ico"/>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#00D9FF" />
-          <meta name="description" property="og:description" content="Stocker AI App is an Investing Advisory App that helps you design strategies and practise them with virtual trading! Learn how to make money and earn passive income with Stocker Ai App" />
-          <meta name="description" content="Stocker AI App is an Investing Advisory App that helps you design strategies and practise them with virtual trading! Learn how to make money and earn passive income with Stocker Ai App" />
+          <meta name="description" property="og:description" content="Design budgeting and investing strategies your AI Financial Advisor and practise them with virtual trading! Learn how to make money and earn passive income with HelloStocker Ai App"/>
+          <meta name="description" content="Design budgeting and investing strategies your AI Financial Advisor and practise them with virtual trading! Learn how to make money and earn passive income with HelloStocker Ai App"/>
           <meta property="og:title" content="Design strategies with your AI financial advisor and practise virtual investing: Make money and take control over your own success" />
           <meta name="og:description" content="Make Money with your Investing Advisor available 24/7! A financial bot and improved version of ChatGPT! Start building your passive income and make money online! Build your brand in Stocker App with all the money you need! powered by ChatGPT bot advise - Start building your passive income and make money online!  Join a community of live and transparent investors and find the best trades with free financial analytics, Speak with your ChatGPT bot advisor! Find the best stocks and cryptocurrency" />
           <meta property="og:type" content="video.movie" />
           <meta property="og:url" content="https://www.hellostocker.com/" />
           <meta property="og:image" content="https://i.ibb.co/3TmgmYC/hellostocker-stocker-app-ai-financial-advisor-budgeting-investing.png"  />
           <meta name="twitter:title" content="Design strategies with your AI financial advisor and practise virtual investing: Make money and take control over your own success"/>
-          <meta name="twitter:description" content="Stocker AI App is an Investing Advisory App that helps you design strategies and practise them with virtual trading! Learn how to make money and earn passive income with Stocker Ai App" />
+          <meta name="twitter:description" content="Design budgeting and investing strategies your AI Financial Advisor and practise them with virtual trading! Learn how to make money and earn passive income with HelloStocker Ai App"/>
           <meta name="twitter:image" content="https://i.ibb.co/3TmgmYC/hellostocker-stocker-app-ai-financial-advisor-budgeting-investing.png"  />
           <meta name="twitter:card" content="summary_large_image"/>
           <link rel="apple-touch-icon" href="https://i.ibb.co/3TmgmYC/hellostocker-stocker-app-ai-financial-advisor-budgeting-investing.png"  />
-          <title>HelloStocker App is an Investing Advisory AI driven App that helps you design strategies and practise them with virtual trading! Learn how to make money and earn passive income with Stocker Ai App</title>
             <script src="mathjax-config.js" defer></script>
                 <script type="text/javascript" id="MathJax-script" defer
                   src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
