adb3241d454fb5cc4e5b0115392cd0e897623d50
stocker88 <aness.hussein.ali@icloud.com>
2023-09-28 11:26:33 +0200

    update linkedin

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -7,7 +7,7 @@ const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
       <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
-      <div className="text-xl">{name}</div>
+      <a href="https://www.linkedin.com/in/anesshusseinali/"><div className="text-xl">{name}</div></a>
     </div>
   )
 }
