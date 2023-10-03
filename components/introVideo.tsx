69f7d9333c39dbe52f208808ac478fbb2083294b
stocker88 <aness.hussein.ali@icloud.com>
2023-10-03 12:00:20 +0200

    update index

--- components/introVideo.tsx
+++ components/introVideo.tsx
@@ -5,7 +5,7 @@ const IntroVideo = () => {
 
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h2 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
-        Money Matters
+        Your Investing Companion: Watch and Learn
       </h2>
      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         You have control over your own success
@@ -16,4 +16,4 @@ const IntroVideo = () => {
   )
 }
 
-export default IntroApp
+export default IntroVideo
