edf9245af1e9546180714e0b164f8096078232a4
stocker88 <aness.hussein.ali@icloud.com>
2023-10-03 13:07:44 +0200

    add videos

--- components/sections/MeetingButton.js
+++ components/sections/MeetingButton.js
@@ -94,7 +94,7 @@ const MeetingButton = ({
 <br></br>
 
                      <div style={{ color: 'white',  fontFamily: 'arial', fontSize: "min(28px,max(24px,2.3vw))", fontWeight: 600,lineHeight: 1.3,textShadow: '2px 2px 2px rgba(1, 74, 173, 0.6)' }} >
-                  Don't risk your money, practise with Virtual trading and AI
+                  Transform Your Financial Future: Get Started Today
                      </div>
         <InstallButtonsWithQR/>
 <br></br><br></br>
