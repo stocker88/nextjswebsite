280888e34e854a6ca70f138f613ae60a9ee8e16e
stocker88 <aness.hussein.ali@icloud.com>
2023-10-10 10:15:47 +0200

    size

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -48,6 +48,7 @@ export default function Post({ post, morePosts, preview }: Props) {
                 <meta name="twitter:image" content={'https://www.hellostocker.com'+post.ogImage.url}/>
                 <meta name="twitter:card" content="summary_large_image"/>
                 <script src="mathjax-config.js" defer></script>
+                <link rel="canonical" href={"https://www.hellostocker.com/posts/"+post.slug} />
                 <script type="text/javascript" id="MathJax-script" defer
                    src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
                 </script>
