5f766869b245482327e59d835ae1426d35c007d6
stocker88 <aness.hussein.ali@icloud.com>
2023-10-15 14:10:36 +0200

    size

--- components/sections/FeaturesSplit.tsx
+++ components/sections/FeaturesSplit.tsx
@@ -69,24 +69,17 @@ const FeaturesSplit = ({
 
   const outerClasses = classNames(
     'features-split section',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
     hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
     className
   );
 
   const innerClasses = classNames(
     'features-split-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
   );
 
   const splitClasses = classNames(
     'split-wrap',
     invertMobile && 'invert-mobile',
-    invertDesktop && 'invert-desktop',
-    alignTop && 'align-top'
   );
 
   return (
