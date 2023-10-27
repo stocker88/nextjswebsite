1ae60a94df770e3009d1adea1fc4aa9278764436
stocker88 <aness.hussein.ali@icloud.com>
2023-10-27 16:27:27 +0200

    update title

--- components/swiper/swipe.js
+++ components/swiper/swipe.js
@@ -61,7 +61,7 @@ export default function Swipe() {
    <div className="split-item">
                 <div className="split-item-content reveal-from-left" data-reveal-container=".split-item">
                   <h3 className="text-3xl md:text-6xl font-bold mt-0 mb-12 center-content-mobile" style={{color:'lightgrey', fontFamily: 'arial'}}>
-                      Stocks That Will Blow Up
+                      Stocks That Skyrocket
                     </h3>
 
                 </div>
