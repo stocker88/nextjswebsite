e4489f6f84bbb209d7cb61b88ebaf0bff224b720
stocker88 <aness.hussein.ali@icloud.com>
2023-11-11 13:35:44 +0100

    update title

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -74,7 +74,7 @@ const Hero = ({
                                             HelloStocker
                                           </h1>
               <div style={{ color: 'white', fontSize: "min(40px,max(30px,3vw))", fontFamily: 'arial', fontWeight: 600,lineHeight: 1.3,textShadow: '2px 2px 2px rgba(1, 74, 173, 0.6)' }} className="reveal-from-bottom" data-reveal-delay="150">
-                   Invest with your AI Advisor
+                   Virtual Investing and Trade Signals
 
                 </div>
 <br></br>
--- components/swiper/swipe.js
+++ components/swiper/swipe.js
@@ -31,7 +31,7 @@ export default function Swipe() {
                               <div className="split-item-content reveal-from-left" data-reveal-container=".split-item">
                                 <h3 className="text-3xl md:text-6xl font-bold mt-0 mb-12 center-content-mobile" style={{color:'lightgrey', fontFamily: 'arial'}}>
 
-                                Get Stocks To Buy - DM Your Advisor
+                                Get Stocks To Buy - DM Your AI Advisor
                                   </h3>
 
                               </div>
