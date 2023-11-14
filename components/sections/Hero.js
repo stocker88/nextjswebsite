83eecf1df3ea550c73f833a35cf3714cbe7fc2f4
stocker88 <aness.hussein.ali@icloud.com>
2023-11-14 13:33:40 +0100

    update title

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -71,7 +71,7 @@ const Hero = ({
                   />
             <div className="container-xs">
             <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 2,textShadow: '3px 3px 3px rgba(1, 74, 173, 0.6)' }} >
-                                            HelloStocker AI App
+                                            HelloStocker App
                                           </h1>
               <div style={{ color: 'white', fontSize: "min(40px,max(30px,3vw))", fontFamily: 'arial', fontWeight: 600,lineHeight: 1.3,textShadow: '2px 2px 2px rgba(1, 74, 173, 0.6)' }} className="reveal-from-bottom" data-reveal-delay="150">
                    Virtual Investing & Trading Signals
