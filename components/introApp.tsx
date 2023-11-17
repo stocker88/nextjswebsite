6921fed8e987bf2c9b6ce778ac24f038f420fbd4
stocker88 <aness.hussein.ali@icloud.com>
2023-11-17 02:16:29 +0100

    update title

--- components/introApp.tsx
+++ components/introApp.tsx
@@ -5,7 +5,7 @@ const IntroApp = () => {
 
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h2 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8" style={{'color':'white'}} >
-        Hot Stocks To Buy in 2023
+        Receive Hot Stocks To Buy
       </h2>
      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         You have control over your own success
