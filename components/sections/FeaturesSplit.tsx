5bf4f4348076355a2027177e66e4b6a0e69c0b6b
stocker88 <aness.hussein.ali@icloud.com>
2023-11-17 23:25:17 +0100

    update title

--- components/sections/FeaturesSplit.tsx
+++ components/sections/FeaturesSplit.tsx
@@ -100,7 +100,7 @@ const FeaturesSplit = ({
         <br></br>
         <br></br>
         <br></br>
-        <div style={{ color: 'white', fontSize: "min(40px,max(30px,3vw))", fontFamily: 'arial', fontWeight: 600,lineHeight: 1.3,textShadow: '2px 2px 2px rgba(1, 74, 173, 0.6)' }} className="reveal-from-bottom" data-reveal-delay="150">
+        <div style={{ color: 'white', fontSize: "min(30px,max(20px,2vw))", fontFamily: 'arial', fontWeight: 500,lineHeight: 1,textShadow: '2px 2px 2px rgba(1, 74, 173, 0.6)' }} className="reveal-from-bottom" data-reveal-delay="150">
         Having a coffee break? Chat with your own personal financial advisor let's make money!
         <br></br>
         We send you hot stocks to buy now: find the next Nvidia, Netflix and Tesla before they do +999%
@@ -112,94 +112,96 @@ const FeaturesSplit = ({
         - Wall St analyst ratings
         <br></br>
         - Social media sentiment data on Reddit and Twitter
+        <br></br>
         - Fundamental analysis
+        <br></br>
         - Quantitative analysis
         <br></br>
         You will receive Stock Market Analysis with:
+        <br></br>
         - What the company does
+        <br></br>
         - Why it's growing in interest
+        <br></br>
         - How it could be more valuable
+        <br></br>
         - Risks to watch out for
+        <br></br>
         - What competitors to watch for
+        <br></br>
 
         We predicted these stocks would go up and our users profited over +400% on these names!
-
+        <br></br>
         You get insights you won’t find anywhere else.
-
+        <br></br>
         We use advanced AI to track hundreds of signals and data points to help us filter through the noise and find high quality stocks.
-
+<br></br>
         Unbiased with no hidden agenda.
-
+<br></br>
         Other services are filled with opinions, recommendations, and pump-and-dump stocks. HelloStocker AI reports are 100% unbiased, objective, and data-driven.
-
+<br></br>
         Simple and easy to understand.
-
+<br></br>
         You don’t need another long financial report to read. Each report is a simple concise message with at most 2 market graphs easy to understand.
-
+<br></br>
         You save 80+ hours of research per month.
-
+<br></br>
         Our AI Advisor processed thousands of financial reports, stock earnings results, investor presentations, and 10-Ks every month so you don’t have to.
-
+<br></br>
         Get access to early opportunities.
-
+<br></br>
         No one wants to buy a stock after it’s gone up 56,978%. We look for stocks that still have huge upside according to the data.
-
+<br></br>
         No complicated tools or dashboards.
-
+<br></br>
         Have you ever tried using a Bloomberg terminal? We have used them for decades. All you’ll need to get up to speed is a comfortable chair, 5 minutes of your time, and maybe a cup of coffee: Install HelloStocker now!
-
+<br></br>
         Warren Buffet started his love for stock investing at 11 years old.
-
+<br></br>
         He bought his first stocks during the great depression, and is now a billionaire investors: not millionnaire, billionnaire!
-
+<br></br>
         Start early and Compound Interest to grow your wealth exponentially.
-
+<br></br>
         Want to view what other top traders do?
-
+<br></br>
         Join our awesome community of investors, where you can connect with other pro trades!
-
+<br></br>
         Our community discussed the hottest stocks and cryptos to watch!
-
+<br></br>
         We uses AI to study social hot market trends that make money!
-
+<br></br>
         Stay updated with financial news.
-
+<br></br>
         In HelloStocker you can view top investors trading activity: Cathy Wood predicted Tesla.
-
+<br></br>
         Access their Portfolio Positions and Replicate them with Virtual Trading.
-
+<br></br>
         A Virtual Trading App to practice making money without any risk.
-
+<br></br>
         You will have unlimited Virtual Money to try our AI Analysis.
-
+<br></br>
         Advisory AI is the future of investing.
-
+<br></br>
         Artificial Intelligence and Big Data generate smart trading decisions by processing millions of data humans can't.
-
+<br></br>
         Our user front AI is Powered By Open AI ChatGPT GPT4 model.
-
-        We send you the processed data in interactive analysis:
-        - Find high earnings growth rates stocks,
-        - Buy dividend stocks with +20% dividend yield,
-        - Access discounted value stock analysis,
-        - Receive market Price predictions based on Reddit & other social platform analysis,
-        - Access stock price forecast from over 1,200 Portfolio Analyst Researchers.
-
+<br></br>
         Have extra time to search and learn yourself?
-
+<br></br>
         Use our AI investment screener to learn investing and improve your trading skills!
-
+<br></br>
         Trading Strategies are built in for you.
-
+<br></br>
         One click and the AI model will show you a menu of stocks to buy based on what you want!
-
+<br></br>
         Make money now with your AI Investment Advisor powered by ChatGPT.
-
+<br></br>
         HelloStocker can help you achieve financial freedom and early retirement with passive income revenues.
-
+<br></br>
         Start early: compound interest grows your wealth exponentially.
         </div>
         <br></br>
+<center><InstallButtonsWithQRAndLogo style={{ background: "linear-gradient(#4B6DFD, #617EF6, #FF79FA)"}}/></center>
 
         <br></br>
         <br></br>
