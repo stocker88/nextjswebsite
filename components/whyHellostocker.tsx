d044e123f75f46201b86692140ea0ac3c8159d5c
stocker88 <aness.hussein.ali@icloud.com>
2023-11-19 20:46:47 +0100

    update title

--- components/whyHellostocker.tsx
+++ components/whyHellostocker.tsx
@@ -5,7 +5,7 @@ const WhyHellostocker = () => {
 
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h2 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8" style={{'color':'white', 'textShadow': '2px 2px 2px rgba(0, 0, 0, 0)' }} >
-        10 Ways Your AI Advisor Can Benefit You
+        10 Ways Your AI Advisor Benefits You
       </h2>
      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         You have control over your own success
