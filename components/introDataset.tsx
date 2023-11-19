539384c03f3ca6831a8cae5bae500709b71caefc
stocker88 <aness.hussein.ali@icloud.com>
2023-11-19 23:34:09 +0100

    size

--- components/introDataset.tsx
+++ components/introDataset.tsx
@@ -5,7 +5,7 @@ const IntroDataset = () => {
 
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h2 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8" style={{'color':'white', 'textShadow': '2px 2px 2px rgba(0, 0, 0, 0)' }} >
-        We Give You Access To Pro Data
+        You Get Access To Pro Data
       </h2>
      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         You have control over your own success
