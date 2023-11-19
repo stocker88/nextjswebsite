dc53c317c65f4cf08f5572e982f344972b5b4da6
stocker88 <aness.hussein.ali@icloud.com>
2023-11-19 20:55:51 +0100

    update title

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -71,12 +71,15 @@ const Hero = ({
                   />
             <div className="container-xs">
             <h1 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 2,textShadow: '3px 3px 3px rgba(1, 74, 173, 0.6)' }} >
+                                            What Stocks To Buy Now?
+                                          </h1>
+<h1 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 2,textShadow: '3px 3px 3px rgba(1, 74, 173, 0.6)' }} >
                                             You Should Ask Your AI Advisor
                                           </h1>
 
 
                         <h1 className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 2,textShadow: '3px 3px 3px rgba(1, 74, 173, 0.6)' }} >
-                             We give you $10,000 Virtual Money to Practise Trading Ideas
+                             He gives you $10,000 Virtual Money to Practise Trading Ideas
                              </h1>
 
                               <Image
