63b27d350a9d50d25d4193248c9930ec8626029a
stocker88 <aness.hussein.ali@icloud.com>
2023-11-20 16:42:21 +0100

    size

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -95,7 +95,7 @@ const Hero = ({
                                         <br></br>
 
                 <InstallButtonsWithQR/>
-                                                        <h1 className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 1.3,textShadow: '3px 3px 3px rgba(1, 74, 173, 0.6)' }} >
+                                                        <h1 className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 1.3,textShadow: '3px 3px 3px rgba(0, 0, 0, 0)' }} >
                                                                                      Install Now And Get $10,000 Virtual Trading Money
                                                                                      </h1>
                                                                           <br></br>
