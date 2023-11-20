2f17b6ec3a4d0c82f44801df9e3904cba1062dbc
stocker88 <aness.hussein.ali@icloud.com>
2023-11-20 21:14:43 +0100

    size

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
       fontSize: {
         '5xl': '2.5rem',
         '6xl': '2.75rem',
-        '7xl': '4.5rem',
+        '7xl': '3.5rem',
         '8xl': '6.25rem',
       },
       boxShadow: {
