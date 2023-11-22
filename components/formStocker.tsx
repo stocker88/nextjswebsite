169e6c2d3680b528e5ff78cd75d254ef06defc50
stocker88 <aness.hussein.ali@icloud.com>
2023-11-22 14:31:04 +0100

    working

--- components/formStocker.tsx
+++ components/formStocker.tsx
@@ -164,6 +164,7 @@ style={{'padding': '10px'}}>
                                                    'paddingBottom': '16px',
                                                    'color': 'white',
                                                     'width':'30vw',
+                                                    'display': 'flex', 'justifyContent':'flex-end'
                                                    'textAlign': 'center',
                                                    'fontSize': 'min(19px,max(15px,2vw))',
                                                    'borderRadius': '30px 30px 30px 30px',
