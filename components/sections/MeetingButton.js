581b9fdcc0ec37ae1c9213582d21086fc3d327b6
stocker88 <aness.hussein.ali@icloud.com>
2023-11-22 10:34:03 +0100

    working

--- components/sections/MeetingButton.js
+++ components/sections/MeetingButton.js
@@ -73,8 +73,47 @@ const MeetingButton = ({
                      </div>
                        <br></br>
                       <br></br>
-        <InstallButtonsWithQR/>
-<br></br><br></br>
+           <h2  style={{ color: 'lightGrey',fontWeight: 600, fontFamily: 'arial',fontSize: "min(18px,max(16px,3vw))", lineHeight: 1.3,textShadow: '4px 4px 4px rgba(1, 74, 173, 0.6)', maxWidth: "700px" }} >
+                    HelloStocker helps you find stocks before they blow up. Save time, save money and receive personalized AI recommendations based on your own tastes.
+                    </h2>
+                  </div>
+
+                </div>
+
+         </div>
+         </center>
+         </Container>
+        <NewsletterSignUp/>
+        <br></br>
+        <Container>
+
+
+
+        <center>
+            <Image
+                src='/assets/images/usersRating.webp'
+                alt="AI Financial Advisor & Virtual Investing App sends you hot stocks to watch and buy now: find the next Nvidia, Tesla, Roblox before they skyrocket"
+                style={{width:240, height:60}}
+            />
+
+            <div style={{ color: 'lightGrey', fontSize: "min(16px,max(15px,1.5vw))", fontFamily: 'arial', lineHeight: 1,maxWidth: "600px",textShadow: '5px 5px 5px rgba(0, 0, 0, 0)' }} className="reveal-from-bottom" data-reveal-delay="150">
+                Join +12,800 investors using HelloStocker AI to receive investment recommendations adapted to their needs.
+            </div>
+
+            <div style={{paddingTop: 20}}>
+            </div>
+            <br></br>
+
+          <div className="container-sm has-animations" style={{  zIndex: 10 }}>
+              <div className="hero-content">
+
+            <Image
+            src='/assets/images/chatgptgooglecloud.webp'
+            alt="Google Cloud Platform Security and ChatGPT Open AI partners logos"
+           style={{width:195.9375, height:83.98}}
+            />
+        <br></br>
+        <br></br>
     </center>
 </Container>
 </section>
