7cc041adb3b2c5dfc5a1c10ef825ba2e4a4f97aa
stocker88 <aness.hussein.ali@icloud.com>
2023-11-24 00:58:37 +0100

    add testimonies

--- components/sections/Testimonial.js
+++ components/sections/Testimonial.js
@@ -13,35 +13,20 @@ const defaultProps = {
 }
 
 const Testimonial = ({
-  className,
-  topOuterDivider,
-  bottomOuterDivider,
-  topDivider,
-  bottomDivider,
-  hasBgColor,
-  invertColor,
-  pushLeft,
   ...props
 }) => {
 
   const outerClasses = classNames(
     'features-tiles section',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
     className
   );
 
   const innerClasses = classNames(
     'testimonial-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
   );
 
   const tilesClasses = classNames(
     'tiles-wrap',
-    pushLeft && 'push-left'
   );
 
   const sectionHeader = {
