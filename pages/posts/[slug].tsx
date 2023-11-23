d5c04a6369e38114bf5c4439f2b82876df7d4281
stocker88 <aness.hussein.ali@icloud.com>
2023-11-23 18:36:45 +0100

    size

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -11,6 +11,7 @@ import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 import type PostType from '../../interfaces/post'
+import MoreStories from '../../components/more-stories'
 
 type Props = {
   post: PostType
