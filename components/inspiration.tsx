f9c5cdb7e09853b58e6d56a5bb2e05c350ac52d6
stocker88 <aness.hussein.ali@icloud.com>
2023-11-28 01:30:49 +0100

    add inspiration

--- components/inspiration.tsx
+++ components/inspiration.tsx
@@ -5,19 +5,15 @@ const QuoteDisplay = () => {
   const [quotes, setQuotes] = useState([
   "Opportunities in the stock market are like sunrises; if you wait too long, you'll miss them. - Warren Buffett",
     "If you don't find a way to make money while you sleep, you will work until you die. - Warren Buffett",
-    "Investing is about using your money to make more money. - Robert Kiyosaki",
-    "Risk comes from not knowing what you're doing. - Warren Buffett",
-    "The stock market is a device for transferring money from the impatient to the patient. - Warren Buffett",
+    "Use your money to make more money. - Robert Kiyosaki",
+    "Risk comes from not knowing what you're doing, always double check with an advisor. - Warren Buffett",
+    "The stock market is a device for making the money work. - Warren Buffett",
     "Price is what you pay. Value is what you get. - Warren Buffett",
-    "In the short run, the market is a voting machine, but in the long run, it is a weighing machine. - Benjamin Graham",
-    "The only thing we know about the stock market is that it will fluctuate. – Benjamin Graham",
-    "Investing should be more like watching paint dry or watching grass grow. If you want excitement, take $800 and go to Las Vegas. – Paul Samuelson",
-    "The stock market is a teacher; its lessons can be both rewarding and costly. - Benjamin Graham",
     "Your net worth is determined by your network – the people you learn from and grow with. - Warren Buffett",
     "A well-diversified portfolio is the shield that guards your financial future. - Benjamin Graham",
     "The stock market doesn't discriminate; it's open to anyone willing to learn and take action. - Warren Buffett",
     "Success in investing is built on a foundation of sound research and thoughtful decisions. - Warren Buffett",
-    "The market is a giant discount machine. It's the only store where when everything goes on sale, people run out of the store. – Mark Cuban",
+    "The market is a giant discount machine. When everything goes on sale, people run out of the store. – Mark Cuban",
     // Add more quotes here as needed
   ]);
 
@@ -25,7 +21,7 @@ const QuoteDisplay = () => {
 
   const generateRandomQuote = () => {
     const newIndex = Math.floor(Math.random() * quotes.length);
-    setCurrentQuoteIndex((currentQuoteIndex + 1)%quotes.length);
+    setCurrentQuoteIndex((newIndex)%quotes.length);
   };
 
 
