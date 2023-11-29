89cc61362642354fc09cd74f0e31cf95d16fb60d
stocker88 <aness.hussein.ali@icloud.com>
2023-11-29 19:12:55 +0100

    add inspiration

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -119,11 +119,7 @@ const Hero = ({
        style={{width:195.9375, height:83.98}}
         />
                 <br></br>
-
-                <div style={{ color: 'lightGrey', fontSize: "min(16px,max(15px,1.5vw))", fontFamily: 'arial', lineHeight: 1,maxWidth: "600px",textShadow: '5px 5px 5px rgba(0, 0, 0, 0)' }} className="reveal-from-bottom" data-reveal-delay="150">
-                                                               Unlock the Power of Big Data: we give you $10,000 to test ideas in Virtual Reality Trading
-                                                             </div>
-                                                                             <br></br>
+             <br></br>
                 <br></br>
                 <br></br>
                                           <br></br>
