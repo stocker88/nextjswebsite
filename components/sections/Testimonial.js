17f39458706eec57e7b297e6caccf1513e02d569
stocker88 <aness.hussein.ali@icloud.com>
2023-11-30 14:32:58 +0100

    add inspiration

--- components/sections/Testimonial.js
+++ components/sections/Testimonial.js
@@ -114,7 +114,11 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Simple and Easy to Understand </b>
                              <br></br>
-                             "I wanted dividend stocks for passive income and received a clear reply straight to the point. Finally financial advises that don't require a finance degree... HelloStocker delivers concise messages with easy-to-read graphs and user friendly ChatGPT model interface. No more financial jargon."
+                             "I wanted dividend stocks for passive income and received a clear reply straight to the point.
+                             <br></br>
+                             Finally financial advises that don't require a finance degree...
+                             <br></br>HelloStocker delivers concise messages with easy-to-read graphs and user friendly ChatGPT model interface.
+                             <br></br>No more financial jargon."
                               <br></br>
                               <br></br>
                          <b>  Fabien Nieto
@@ -140,7 +144,10 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Saves 80+ Hours of Investing Research per Month </b>
                              <br></br>
-                             "HelloStocker App my time-saver hero! Instead of spending hours shifting through boring emails and reports, I get instant suggestions and replies that fit my preferences, right away. Time is money in trading, I can't afford to wait for email replies. After spending a couple of months practising with their virtual trading, I got a sense of how biotech works and made 3 digit profits!"
+                             "HelloStocker App my time-saver hero!
+                             <br></br>Instead of spending hours shifting through boring emails and reports, I get instant suggestions and replies that fit my preferences, right away.
+                             <br></br>Time is money in trading, I can't afford to wait for email replies.
+                             <br></br>After spending a couple of months practising with their virtual trading, I got a sense of how biotech works and made 3 digit profits!"
                               <br></br>
                               <br></br>
                          <b>  Felix Revert
@@ -166,7 +173,11 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Access Early Trading Opportunities </b>
                              <br></br>
-                             "Being ahead of the curve matters: time is money. HelloStocker Financial Advisor spots stocks with huge upside potential before the masses catch on. They notify you directly on your phone: you receive the information faster. That early advantage has made a real difference in my gains. I perfectly timed Adobe, AMD & Workforce."
+                             "Being ahead of the curve matters: time is money.
+                             <br></br>HelloStocker Financial Advisor spots stocks with huge upside potential before the masses catch on.
+                              <br></br>They notify you directly on your phone: you receive the information faster.
+                               <br></br>That early advantage has made a real difference in my gains.
+                                <br></br>I perfectly timed Adobe, AMD & Workforce."
                               <br></br>
                               <br></br>
                          <b>  Alexia Karolewicz
@@ -192,7 +203,10 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Powered by ChatGPT </b>
                              <br></br>
-                             "No more complicated Bloomberg terminals! Old school way! HelloStocker is the new way to go: the ChatGPT of investing. They make it easy to navigate the large stock universe. I received blue chip stocks recommendations that keep outperforming the sp500. It's easy to make money when the market goes up, but it's always hard to beat the sp500 market, well done team!"
+                             "No more complicated Bloomberg terminals! Old school way! HelloStocker is the new way to go: the ChatGPT of investing.
+                              <br></br>They make it easy to navigate the large stock universe.
+                              <br></br>I received blue chip stocks recommendations that keep outperforming the sp500.
+                              <br></br>It's easy to make money when the market goes up, but it's always hard to beat the sp500 market, well done team!"
                               <br></br>
                               <br></br>
                          <b>  Mehrad Oroudji
@@ -218,7 +232,11 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Join a Pro Community of Investors </b>
                              <br></br>
-                             "HelloStocker community is a goldmine! Investors and hedge fund members has given me financial insights and great connections, they are here to help. They don't push me to trade, in fact you can't real trade with them at all... They have my long term interests in mind and help me train with their tools. eToro became a naughty mess they should rethink their UX and get rid of all these influencers who want to make money by pushing you to buy anything now. Above all HelloStocker AI Advisor is a must: saves me time and my profits increased!"
+                             "HelloStocker community is a goldmine! Investors and hedge fund members has given me financial insights and great connections, they are here to help.
+                             <br></br>They don't push me to trade, in fact you can't real trade with them at all...
+                              <br></br>They have my long term interests in mind and help me train with their tools.
+                              <br></br>eToro became a naughty mess they should rethink their UX and get rid of all these influencers who want to make money by pushing you to buy anything now.
+                              <br></br>Above all HelloStocker AI Advisor is a must: saves me time and my profits increased!"
                               <br></br>
                               <br></br>
                          <b>  Charles Elie Labroy
@@ -244,7 +262,9 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Exponential Growth Potential </b>
                              <br></br>
-                             "It's not just about investing, it's about building wealth, and making it grow the smart way. I don't just 'realise profits': I track how much I make over the sp500. Making money is easy when everything goes up, beating the sp500 market with their Stocker AI advisor is the real way to go!"
+                             "It's not just about investing, it's about building wealth, and making it grow the smart way.
+                              <br></br>I don't just 'realise profits': I track how much I make over the sp500.
+                              <br></br>Making money is easy when everything goes up, beating the sp500 market with their Stocker AI advisor is the real way to go!"
                               <br></br>
                               <br></br>
                          <b>  Kanishk Budhia
@@ -270,7 +290,10 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> A Safe Playground for learning </b>
                              <br></br>
-                             "Practise trading ideas without the risk! HelloStocker App nails it. They don't take any money from you, it's not about pushing you to trade now: it's about making sure you acquire the skillset and profit from it. I get as much virtual trading money as I want and can practice trading ideas without the fear of losing real cash. It's a safe playground for learning and gaining confidence in their AI technology."
+                             "Practise trading ideas without the risk! HelloStocker App nails it.
+                             <br></br>They don't take any money from you, it's not about pushing you to trade now: it's about making sure you acquire the skillset and profit from it.
+                              <br></br>I get as much virtual trading money as I want and can practice trading ideas without the fear of losing real cash.
+                              <br></br>It's a safe playground for learning and gaining confidence in their AI technology."
                               <br></br>
                               <br></br>
                          <b>  Maxime Lonné
@@ -297,7 +320,10 @@ const Testimonial = ({
 
                               <b style={{color:'#00f75f'}}> Access Transparent AI Data </b>
                              <br></br>
-                             "HelloStocker AI doesn't hide a thing, I get access to AI-processed data and can verify and understand how the decisions are made. Transparency like this builds trust. I understand how my ai advisor takes the decisions and can interact with the AI directly, better than sending emails and waiting for answers, makes me save time and cash on profits faster. Time is money especially in investing! Stocker App great job!"
+                             "HelloStocker AI doesn't hide a thing, I get access to AI-processed data and can verify and understand how the decisions are made.
+                              <br></br>Transparency like this builds trust.
+                              <br></br>I understand how my ai advisor takes the decisions and can interact with the AI directly, better than sending emails and waiting for answers, makes me save time and cash on profits faster.
+                              <br></br>Time is money especially in investing! Stocker App great job!"
                               <br></br>
                               <br></br>
                          <b>  Sacha Berenfus
