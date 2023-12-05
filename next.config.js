ebbfc1281de3fe1822c68552339ea143b325f5f0
stocker88 <aness.hussein.ali@icloud.com>
2023-12-05 01:18:37 +0100

    update markdown

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
-// next.config.js
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        // Define caching headers for _next/static directory
-        source: "/_next/static/:slug*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+module.exports = (phase) => {
+  const isProduction = phase === PHASE_PRODUCTION_BUILD;
+
+  return {
+    assetPrefix: isProduction ? 'https://www.hellostocker.com/' : '', // Replace with your CDN URL
+    generateBuildId: async () => {
+      // Custom build ID to include a content hash
+      if (isProduction) {
+        return require('crypto').randomBytes(16).toString('hex');
+      }
+    },
+    // Other Next.js configurations...
+  };
 };
