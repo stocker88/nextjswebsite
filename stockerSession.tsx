994f2c8597d5312f335a7cea3a1f713f1d13b10f
stocker88 <aness.hussein.ali@icloud.com>
2023-12-11 20:42:04 +0100

    tmp

--- stockerSession.tsx
+++ stockerSession.tsx
@@ -1,5 +1,12 @@
 import React, { createContext, useState, useContext } from 'react';
 
+interface SessionData {
+  email: string;
+  sessionId: string;
+  // ... other properties
+}
+
+
 const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
@@ -12,7 +19,7 @@ export const SessionProvider = ({ children }) => {
   );
 };
 
-export const useSession = () => {
+export const useSession = : { sessionData: SessionData; setSessionData: React.Dispatch<React.SetStateAction<SessionData>> } =>  {
   const context = useContext(SessionContext);
   if (!context) {
     throw new Error('useSession must be used within a SessionProvider');
