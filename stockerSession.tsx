4ebc443b2a304ef4adb93ce3f1d0ed7fc6ac4ef4
stocker88 <aness.hussein.ali@icloud.com>
2023-12-11 22:28:25 +0100

    tmp

--- stockerSession.tsx
+++ stockerSession.tsx
@@ -13,7 +13,7 @@ interface SessionContextProps {
 
 const SessionContext = createContext<SessionContextProps | null>(null);
 
-export const SessionProvider: ({ preview, children }: Props) => {
+export const SessionProvider: ({ children }: Props) => {
   const [sessionData, setSessionData] = useState<SessionData>({ email: '', sessionId: '' });
 
   return (
