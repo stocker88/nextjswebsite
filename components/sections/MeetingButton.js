baeb310c74548d5be4c560ac1621c86f4ca0d599
stocker88 <aness.hussein.ali@icloud.com>
2023-12-14 13:43:27 +0100

    tmp

--- components/sections/MeetingButton.js
+++ components/sections/MeetingButton.js
@@ -111,7 +111,7 @@ const MeetingButton = ({
             />
 
             <div style={{ color: 'lightGrey', fontSize: "min(16px,max(15px,1.5vw))", fontFamily: 'arial', lineHeight: 1,maxWidth: "600px",textShadow: '5px 5px 5px rgba(0, 0, 0, 0)' }} className="reveal-from-bottom" data-reveal-delay="150">
-                Share ideas with +12,800 investors in the best social virtual trading app
+                Join +12,800 investors and receive the best stocks & crypto today
             </div>
 
             <div style={{paddingTop: 20}}>
