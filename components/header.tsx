885d97e14e0eb04749c60ed649ad55e786ac31a9
stocker88 <aness.hussein.ali@icloud.com>
2023-12-14 13:38:29 +0100

    tmp

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,107 @@
 import Link from 'next/link'
 
+import { doc, setDoc } from "firebase/firestore";
+import 'firebase/firestore';
+import {getFirestore} from 'firebase/firestore';
+import { serverTimestamp } from "firebase/firestore";
+import { useSession } from '../stockerSession';
+import { useState, useEffect } from "react";
+import { format } from 'date-fns';
+
+
+
 const Header = () => {
+
+
+
+const db = getFirestore();
+    const { sessionData, setSessionData } = useSession();
+        const [deviceInfo, setDeviceInfo] = useState({
+            userAgent: '',
+            platform: '',
+            language: '',
+            screenWidth: '',
+            screenHeight: '',
+        });
+
+ const fetchDeviceInfo = () => {
+        try {
+          // Get basic device information
+          setDeviceInfo({
+            ...deviceInfo,
+            userAgent: navigator.userAgent,
+            platform: navigator.platform,
+            language: navigator.language,
+            screenWidth: String(window.screen.width),
+            screenHeight: String(window.screen.height),
+            // Add or update other properties as needed
+          });
+        } catch (error) {
+          console.error('Error fetching device information:', error);
+
+          // Set empty string ('') as default values if fetching device information fails
+          setDeviceInfo({
+            ...deviceInfo,
+            userAgent: '',
+            platform: '',
+            language: '',
+            screenWidth: '',
+            screenHeight: '',
+            // Set other properties to empty strings as needed
+          });
+        }
+      };
+
+
+
+  useEffect(() => {
+        // Fetch device information when the component mounts
+        fetchDeviceInfo();
+      }, []); // Empty dependency array ensures the effect runs only once after initial render
+
+
+  const handleClick = () => {
+    const dateTime = Date.now();
+    const unixTime = Math.floor(dateTime / 1000);
+
+    const email = sessionData.email || '';
+    const sessionId = sessionData.sessionId || '';
+
+
+  const randomString = Math.random().toString(20).substring(2, 14) + Math.random().toString(20).substring(2, 14);
+    const docId = email === ''
+      ?  sessionId ===''?
+        deviceInfo.language + deviceInfo.platform + deviceInfo.screenWidth + deviceInfo.screenHeight+randomString
+        : sessionId
+      : email;
+    if (email === ''){
+        setSessionData({ ...sessionData, sessionId: docId });
+    }
+
+     setDoc(doc(db, "contactList", docId), {
+        [ `timeViewClickedPost${slug}` ]: serverTimestamp(),
+        time: serverTimestamp(),
+         [ `unixTimeViewClickedPost${slug}` ]: unixTime,
+          userAgent: deviceInfo.userAgent,
+          platform: deviceInfo.platform,
+          language: deviceInfo.language,
+          screenWidth: deviceInfo.screenWidth,
+          screenHeight: deviceInfo.screenHeight,
+        }, { merge: true });
+  };
+
+
+
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-10 mt-8">
       <br></br>
         <center>
-      <Link href="/" className="hover:underline" style={{'color':'white', 'textShadow': '2px 2px 2px rgba(0, 0, 0, 0)' }}>
+      <Link
+      href="/"
+      className="hover:underline"
+      style={{'color':'white', 'textShadow': '2px 2px 2px rgba(0, 0, 0, 0)' }}
+      onClick={handleClick}
+      >
         Back to HelloStocker AI
       </Link>
         </center>
