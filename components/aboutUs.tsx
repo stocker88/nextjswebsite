76b520607eeced6db6412d8e62b797f3eddc5e23
stocker88 <aness.hussein.ali@icloud.com>
2024-01-01 18:09:37 +0100

    reorder

--- components/aboutUs.tsx
+++ components/aboutUs.tsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
 
     <section className="flex-col  flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h2 className="text-3xl md:text-6xl font-bold tracking-tighter leading-tight " style={{'color':'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)' }} >
-        Join a Community with AI Trading Tools
+        Join a Community empowered with AI
       </h2>
      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         You have control over your own success
