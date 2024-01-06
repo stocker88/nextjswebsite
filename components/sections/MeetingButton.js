df8bf10246470efb60a7c1aedc494df40721de2c
stocker88 <aness.hussein.ali@icloud.com>
2024-01-06 21:43:09 +0100

    improve web app version

--- components/sections/MeetingButton.js
+++ components/sections/MeetingButton.js
@@ -129,6 +129,43 @@ const MeetingButton = ({
 
         </center>
         </div>
+
+
+
+
+
+
+        <Container>
+
+        <center>
+              <div className="container-sm has-animations" style={{  zIndex: 10 }}>
+                  <div className="hero-content">
+             <div style={{paddingTop: 'max(70px,5vw)'}}></div>
+
+                 {/*    <Image
+                      src={require('assets/images/stockstobuynow.png')}
+                      alt="StocksToBuyNow app stocks to buy ai stocks to watch market financial advisor economic tools investing virtual trading"
+                      width={700}
+                      height={58.33}
+                      />*/}
+
+          <Image
+                          src='/assets/images/stockstobuynow.webp'
+                          alt="StocksToBuyNow AI App"
+                          priority={true}
+                          width={256}
+                          height={256}
+                         style={{
+                         width:'min(max(165px,14vw),250px)',
+                          height:'min(max(165px,14vw),250px)',
+                          }}
+
+                          />
+                    <div className="container-xs">
+                    <br></br>
+                    <h1 className="text-4xl md:text-7xl tracking-tighter leading-tight" style={{ color: 'white', fontFamily: 'arial',lineHeight: 1.2,textShadow: '0px 0px 7px rgba(1, 0, 1, 0.7)', maxWidth: "800px" }} >
+                        Wish someone told you about Nvidia when it was <b>$50 instead of $500</b>?
+                      </h1>
         <div style={{paddingTop: 'max(25px,2.2vw)'}}></div>
                    <h2  style={{ color: 'white',fontWeight: 600, fontFamily: 'arial',fontSize: "min(20px,max(18px,3.5vw))", lineHeight: 1.3,textShadow: '0px 0px 5px rgba(1, 0, 1, 0.7)', maxWidth: "650px" }} >
                             We send you hot stocks to buy before they blow up: you save time, improve your trading, and check the best opportunities.
