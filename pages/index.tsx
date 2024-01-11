16c7269bc50fd5b9cf8693914c47ad74e4e84188
stocker88 <aness.hussein.ali@icloud.com>
2024-01-11 21:07:30 +0100

    add pixel

--- pages/index.tsx
+++ pages/index.tsx
@@ -162,6 +162,27 @@ export default function Index({ allPosts }: Props) {
           <meta name="twitter:card" content="summary_large_image"/>
           <link rel="apple-touch-icon" href="https://i.ibb.co/5hZHsP6/best-stocks-to-buy-now-ai.png"  />
           {/*<script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/143613046.js"></script>*/}
+
+
+          <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+           fbq('init', '3644450535825105');
+          fbq('track', 'PageView');
+          </script>
+          <noscript>
+           <img height="1" width="1"
+          src="https://www.facebook.com/tr?id=3644450535825105&ev=PageView
+          &noscript=1"/>
+          </noscript>
+
+          
         </Head>
 
       </Layout>
