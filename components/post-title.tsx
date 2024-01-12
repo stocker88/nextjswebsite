bd1769e8b19e2c748ed73416992ed3340c989170
stocker88 <aness.hussein.ali@icloud.com>
2024-01-13 00:35:31 +0100

    add pixel

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -7,7 +7,7 @@ type Props = {
 const PostTitle = ({ children }: Props) => {
   return (
     <center>
-    <h1 className="text-5xl md:text-8xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-center" style={{'color':'white'}}>
+    <h1 className="text-3xl md:text-8xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-center" style={{'color':'white'}}>
       {children}
     </h1>
     </center>
