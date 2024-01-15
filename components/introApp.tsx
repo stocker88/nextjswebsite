caef405a68c40ae631dbcbf9f5f8cf24ea52f538
stocker88 <aness.hussein.ali@icloud.com>
2024-01-15 01:42:18 +0100

    add pixel

--- components/introApp.tsx
+++ components/introApp.tsx
@@ -5,9 +5,9 @@ const IntroApp = () => {
 
     <section className="flex-col  flex items-center md:justify-between mt-16 mb-16 md:mb-12">
 
-
+<center>
       <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight " style={{'color':'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)' }} >
-        Receive AI Trading Signals With Explanations
+        Receive AI Trading Signals & Explanations
         </h2>
         <br></br>
         <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight " style={{'color':'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)' }} >
@@ -19,7 +19,7 @@ const IntroApp = () => {
         <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight " style={{'color':'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)' }} >
               Past AI Stock Price Predictions:
               </h2>
-
+</center>
 
 
 
