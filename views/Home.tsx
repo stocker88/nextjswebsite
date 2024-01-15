f6264728232668a33a59e937e72d73d737103911
stocker88 <aness.hussein.ali@icloud.com>
2024-01-15 03:13:22 +0100

    add pixel

--- views/Home.tsx
+++ views/Home.tsx
@@ -135,7 +135,7 @@ import SignUpModal from '../SignUpModal';
                                <br></br>
                                <br></br>
                                <section className="flex-col  flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-<h2 className="text-xl md:text-3xl font-bold tracking-tighter leading-tight" style={{ 'color': 'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)', 'textAlign': 'center' }}>
+<h2 className="text-xl md:text-4xl font-bold tracking-tighter leading-tight" style={{ 'color': 'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)', 'textAlign': 'center' }}>
           We leverage Statistical Methods & AI:
           <br></br>
           <br></br>
