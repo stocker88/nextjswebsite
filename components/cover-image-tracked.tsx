a5531b70f10cb4ba26b4145a88175d9faaee58c2
stocker88 <aness.hussein.ali@icloud.com>
2024-01-22 23:07:11 +0100

    add userid

--- components/cover-image-tracked.tsx
+++ components/cover-image-tracked.tsx
@@ -71,13 +71,13 @@ function CoverImageTracked ({ title, src, slug }: Props) {
 
     const sessionId = sessionData.sessionId || '';
 
-    localStorage.setItem('userId', encodeURIComponent(docId));
+
     const randomString = Math.random().toString(20).substring(2, 14) + Math.random().toString(20).substring(2, 14);
     const docId = sessionId ===''?deviceInfo.language + deviceInfo.platform + deviceInfo.screenWidth + deviceInfo.screenHeight+randomString: sessionId;
     if (sessionId === ''){
         setSessionData({ ...sessionData, sessionId: docId });
     }
-
+    localStorage.setItem('userId', encodeURIComponent(docId));
      setDoc(doc(db, "contactList", docId), {
         timeViewLinkedIn: serverTimestamp(),
         time: serverTimestamp(),
