7947680f6cd7afe43e8648d2c13ffe2cb400585c
stocker88 <aness.hussein.ali@icloud.com>
2024-01-23 20:50:04 +0100

    add userid

--- components/swiper/swipe.js
+++ components/swiper/swipe.js
@@ -98,7 +98,7 @@ const containerStyle = {
         <br></br>
     <section className="flex-col  flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight " style={{'color':'white', 'textShadow': '0px 0px 3px rgba(0, 0, 0, 1)' }} >
-        Should You Buy Tesla? Ask AI for alternatives
+        Should You Buy Tesla? Ask AI for Alternatives
         <br></br>
         <br></br>
         HelloStocker AI Recommended Ferrari instead +28% outperformance
