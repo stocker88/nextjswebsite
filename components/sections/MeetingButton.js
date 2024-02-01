5142d3ca94e16d6e05974d2f961791eba7308aed
stocker88 <aness.hussein.ali@icloud.com>
2024-02-01 07:30:01 +0100

    yo

--- components/sections/MeetingButton.js
+++ components/sections/MeetingButton.js
@@ -119,7 +119,7 @@ const MeetingButton = ({
 
 
     <div style={{ color: 'lightGrey', fontSize: "min(14px,max(13px,1.3vw))", fontFamily: 'arial', lineHeight: 1,maxWidth: "350px",textShadow: '5px 5px 5px rgba(0, 0, 0, 0)' }} className="reveal-from-bottom" data-reveal-delay="150">
-       Join a community of +4,700 investors
+       Join a community of +6,800 investors from Goldman Sachs, Bank of America...
 
        </div>
 
