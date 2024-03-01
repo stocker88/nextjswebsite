8b0dc99e23243d3df94554a1a75a8ded60871957
stocker88 <aness.hussein.ali@icloud.com>
2024-03-01 02:36:28 +0100

    yo

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,4 +1,5 @@
-import {React, useRef} from 'react';
+import React from 'react';
+import {useRef} from 'react';
 // import sections
 import Hero from '../components/sections/Hero';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
